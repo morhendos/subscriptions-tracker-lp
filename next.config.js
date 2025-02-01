23eb4b1dbc5b4e14a5c735be7c253ed14dc9f2d8
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-02-01 20:45:10 +0200

    Remove redundant typescript and eslint config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: false,
-  },
   experimental: {
     typedRoutes: true,
   },
