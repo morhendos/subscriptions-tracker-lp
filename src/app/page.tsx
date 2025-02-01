07b04dc28337021980db0c65b39f72f4d3c315a6
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-02-01 20:43:52 +0200

    Remove unrequested Features component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
 import Hero from '@/components/Hero'
-import Features from '@/components/Features'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
-      <Features />
     </main>
   )
 }
