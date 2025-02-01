6e92cb214b5a184e7e0845c0f2c84dcbbc81ee50
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-02-01 10:36:11 +0200

    Remove HowItWorks section (#2)
    * Remove HowItWorks component as it's not relevant
    
    * Remove HowItWorks import and usage from main page
    
    * d

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import dynamic from 'next/dynamic'
 // Dynamically import client components
 const Hero = dynamic(() => import('@/components/Hero'))
 const Features = dynamic(() => import('@/components/Features'))
-const HowItWorks = dynamic(() => import('@/components/HowItWorks'))
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Hero />
       <Features />
-      <HowItWorks />
     </main>
   )
 }
