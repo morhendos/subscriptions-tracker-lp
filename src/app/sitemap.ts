9165acfcab4ba76426d42f7fc9051697e2c23eec
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-02-06 22:34:24 +0200

    feat: improve SEO (#7)
    * feat: enhance SEO metadata
    
    * feat: add dynamic sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -5,14 +5,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://subscriptions-tracker.com',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'monthly',
       priority: 1,
     },
     {
-      url: 'https://subscriptions-tracker.com/trial',
+      url: 'https://subscriptions-tracker.com/privacy',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
+      changeFrequency: 'yearly',
       priority: 0.8,
     },
+    {
+      url: 'https://subscriptions-tracker.com/terms',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://subscriptions-tracker.com/blog',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.9,
+    }
   ]
 }
