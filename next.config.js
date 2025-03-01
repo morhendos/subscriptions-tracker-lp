937bf132a34f1aee814fc2d82f10eda32c73f61a
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-03-02 00:30:00 +0200

    Update Next.js config to properly handle MongoDB

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  swcMinify: true,
+  // This ensures MongoDB works properly with Next.js
+  webpack: (config, { isServer }) => {
+    // If we're on the server and trying to import MongoDB
+    if (isServer) {
+      // Make sure we don't try to bundle MongoDB in the client
+      config.externals = [...config.externals, 'mongodb'];
+    }
+    
+    return config;
+  },
+};
 
-export default nextConfig
+export default nextConfig;
