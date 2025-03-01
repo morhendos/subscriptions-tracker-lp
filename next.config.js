98ec436e8d352bb65ed508a5f2204af81036009d
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-03-02 00:33:08 +0200

    Restore original Next.js config

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // This ensures MongoDB works properly with Next.js
-  webpack: (config, { isServer }) => {
-    // If we're on the server and trying to import MongoDB
-    if (isServer) {
-      // Make sure we don't try to bundle MongoDB in the client
-      config.externals = [...config.externals, 'mongodb'];
-    }
-    
-    return config;
-  },
 };
 
 export default nextConfig;
