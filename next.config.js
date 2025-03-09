debb0eae8791aaaaa2d9bd0bc5a78e5789b82e60
Tomasz Mikolajewicz <morhendos@gmail.com>
2025-03-09 20:26:02 +0200

    Fix next.config.js to use CommonJS export syntax

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Ensure experimental features are disabled
+  experimental: {
+    appDir: true,
+  }
 }
 
-export default nextConfig
+// Use CommonJS module.exports instead of ES module export
+module.exports = nextConfig
